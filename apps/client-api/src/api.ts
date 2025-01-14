b2d4cc1904c80d24b465251add4f264f5dc96913
omsharma29 <theomsharma100@gmail.com>
2025-01-14 18:26:47 +0530

    turbo-test

---
+++ apps/client-api/src/api.ts
@@ -0,0 +1,26 @@
+import Express from 'express';
+import { Request, Response } from 'express';
+const app = Express();
+const PORT = process.env.PORT || 5000; // Changed default port to 5000
+
+interface MessageResponse {
+  message: string;
+}
+
+app.get('/', async (req: Request, res: Response) => {
+  const response: MessageResponse = {
+    message: "hello testing"
+  };
+  res.json(response);
+});
+
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+process.on('SIGINT', () => {
+  server.close(() => {
+    console.log('Server closed');
+    process.exit(0);
+  });
+});
---
+++ apps/clientweb/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import { Button } from '@repo/ui/button';
+function App() {
+
+  return (
+    <>
+    <div>
+      <Button>HELLO TEST</Button></div>
+    </>
+  )
+}
+
+export default App
